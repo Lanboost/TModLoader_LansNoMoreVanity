bd144ac1bb7434a3c82b5015648778a54832b853
Lanboost <lanboost@hotmail.com>
2019-08-06 20:38:43 +0200

    Added version 1

---
+++ NoMoreVanity.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace NoMoreVanity
+{
+	public class NoMoreVanity : Mod
+	{
+		public NoMoreVanity()
+		{
+		}
+	}
+}
---
+++ NoMoreVanityGlobalItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NoMoreVanity
+{
+    class NoMoreVanityGlobalItem : GlobalItem
+    {
+        public override bool OnPickup(Item item, Player player)
+        {
+            if (item.vanity)
+            {
+
+                WorldGen.EmptyTileCheck
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
